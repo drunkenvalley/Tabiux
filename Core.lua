b81901e30d5c0c42fbdece8fe14848183368025c
Drunkenvalley <drunkenvalley@gmail.com>
2026-04-12 20:00:35 +0200

    Enable predictor again, use string formatter

--- Core.lua
+++ Core.lua
@@ -7,7 +7,6 @@ local tabiux = CreateFrame("Frame", "TabiuxHealth", UIParent, "BackdropTemplate"
 tabiux.target = "player"
 tabiux.height = 36
 tabiux.width = 200
-tabiux.curve = CurveConstants.ScaleTo100
 
 tabiux:SetSize(tabiux.width, tabiux.height)
 tabiux:SetPoint("TOPLEFT", UIParent, "LEFT", 700, -150)
@@ -25,21 +24,20 @@ tabiux.statusbar:SetPoint("TOP", tabiux, "TOP", 0, 0)
 tabiux.statusbar:SetStatusBarTexture("Interface\\ChatFrame\\ChatFrameBackground")
 tabiux.statusbar:SetStatusBarColor(0, 0, 0)
 tabiux.statusbar:SetMinMaxValues(0, 100)
-tabiux.statusbar:SetValue(UnitHealthPercent(tabiux.target, false, tabiux.curve))
+tabiux.statusbar:SetValue(UnitHealthPercent(tabiux.target, false, CurveConstants.ScaleTo100))
 
 tabiux.text = tabiux.statusbar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 tabiux.text:SetPoint("LEFT", tabiux, "LEFT", tabiux.height / 3, 0)
-tabiux.text:SetText(UnitHealthPercent(tabiux.target, false, tabiux.curve) .. "%")
+tabiux.text:SetText(UnitHealthPercent(tabiux.target, false, CurveConstants.ScaleTo100) .. "%")
 
 tabiux.trigger = CreateFrame("Frame", "TabiuxHealthChange", UIParent)
 local function healthChange(self, event, ...)
     if event and event == "UNIT_HEALTH" then
+        self.value = UnitHealthPercent(tabiux.target, true, CurveConstants.ScaleTo100)
         tabiux.text:SetText(
-            C_StringUtil.RoundToNearestString(
-                UnitHealthPercent(tabiux.target, false, tabiux.curve)
-            ) .. "%"
+            string.format("%.0f", self.value) .. "%"
         )
-        tabiux.statusbar:SetValue(UnitHealthPercent(tabiux.target, false, tabiux.curve))
+        tabiux.statusbar:SetValue(self.value)
     end
 end
 
