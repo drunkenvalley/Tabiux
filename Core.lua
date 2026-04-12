516b0812d37590972b79f44961499d769cb64032
Drunkenvalley <drunkenvalley@gmail.com>
2026-04-12 19:42:20 +0200

    Initial commit

---
+++ Core.lua
@@ -0,0 +1,47 @@
+if not TabiuxDb then
+    TabiuxDb = {}
+end
+
+local tabiux = CreateFrame("Frame", "TabiuxHealth", UIParent, "BackdropTemplate")
+
+tabiux.target = "player"
+tabiux.height = 36
+tabiux.width = 200
+tabiux.curve = CurveConstants.ScaleTo100
+
+tabiux:SetSize(tabiux.width, tabiux.height)
+tabiux:SetPoint("TOPLEFT", UIParent, "LEFT", 700, -150)
+tabiux:SetBackdrop({
+    bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+    edgeFile = "Interface\\ChatFrame\\ChatFrameBackground",
+    edgeSize = 1,
+})
+tabiux:SetBackdropColor(.6, .1, .1)
+tabiux:SetBackdropBorderColor(0, 0, 0)
+
+tabiux.statusbar = CreateFrame("StatusBar", "TabiuxHealthbar-" .. tabiux.target, tabiux)
+tabiux.statusbar:SetSize(tabiux.width, tabiux.height - 3)
+tabiux.statusbar:SetPoint("TOP", tabiux, "TOP", 0, 0)
+tabiux.statusbar:SetStatusBarTexture("Interface\\ChatFrame\\ChatFrameBackground")
+tabiux.statusbar:SetStatusBarColor(0, 0, 0)
+tabiux.statusbar:SetMinMaxValues(0, 100)
+tabiux.statusbar:SetValue(UnitHealthPercent(tabiux.target, false, tabiux.curve))
+
+tabiux.text = tabiux.statusbar:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+tabiux.text:SetPoint("LEFT", tabiux, "LEFT", tabiux.height / 3, 0)
+tabiux.text:SetText(UnitHealthPercent(tabiux.target, false, tabiux.curve) .. "%")
+
+tabiux.trigger = CreateFrame("Frame", "TabiuxHealthChange", UIParent)
+local function healthChange(self, event, ...)
+    if event and event == "UNIT_HEALTH" then
+        tabiux.text:SetText(
+            C_StringUtil.RoundToNearestString(
+                UnitHealthPercent(tabiux.target, false, tabiux.curve)
+            ) .. "%"
+        )
+        tabiux.statusbar:SetValue(UnitHealthPercent(tabiux.target, false, tabiux.curve))
+    end
+end
+
+tabiux.trigger:SetScript("OnEvent", healthChange)
+tabiux.trigger:RegisterUnitEvent("UNIT_HEALTH", tabiux.target)
